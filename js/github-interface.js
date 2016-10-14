4750d243b6f5c6dd7929856d19b43f6e722ab4d0
Halle Williams <williams.hr@gmail.com>
2016-10-13 21:10:17 -0700

    add js to display user requested, something wrong with api key, authentication failing

---
+++ js/github-interface.js
@@ -0,0 +1,15 @@
+var apiKey = require('./../.env').apiKey;
+
+$(document).ready(function() {
+  $('#githubUser').click(function() {
+    event.preventDefault();
+    var userName = $('#user').val();
+    $('#user').val("");
+    $('#showRepos').text("The user you have chosen is " + userName + ".");
+    $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+    console.log(response);
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+});
+});

baea62c756c2c5976f2f1257e689849f003e4fc8
Halle Williams <williams.hr@gmail.com>
2016-10-17 00:39:35 -0700

    create prototype and loop to display repos, seperate front and back end

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,15 +1,12 @@
-var apiKey = require('./../.env').apiKey;
+var User = require('./../js/github.js').userModule;
 
 $(document).ready(function() {
   $('#githubUser').click(function() {
     event.preventDefault();
     var userName = $('#user').val();
     $('#user').val("");
-    $('#showRepos').text("The user you have chosen is " + userName + ".");
-    $.get('https://api.github.com/search/repositories?q=user:'+ userName + '&access_token=' + apiKey).then(function(response) {
-     console.log(response);
-  }).fail(function(error){
-    console.log(error.responseJSON.message);
+    $('#showRepos').text("");
+    newUserObject = new User();
+    newUserObject.getRepos(userName);
   });
-});
 });
--- js/github.js
+++ js/github.js
@@ -1,1 +1,15 @@
+var apiKey = require('./../.env').apiKey;
+function User() {
 
+}
+
+User.prototype.getRepos = function(userName){
+  $.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey).then(function(response){
+    for (var i = 0; i < response.length; i++){
+      $("#showRepos").append("<li>" + "Name: " + response[i].name + " // Description: " + response[i].description);
+    }
+  }).fail(function(error){
+  });
+};
+
+exports.userModule = User;
